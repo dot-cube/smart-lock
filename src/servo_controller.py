f50d05ef0b24bc69208e41e0841408ab5a59e24f
Takahiro55555 <tkhr.corei7@gmail.com>
2020-01-31 02:09:09 +0900

    Update: iOSの「ショートカット」アプリからSSH経由で開閉ができるようになった

--- src/servo_controller.py
+++ src/servo_controller.py
@@ -2,6 +2,8 @@ import sys
 import time
 
 import RPi.GPIO as GPIO
+from settings import DETECTION_THRESHOLD
+
 
 def main():
     cmd_crockwise = "cw"
@@ -21,13 +23,13 @@ def main():
             del s_controller
             break
 
+
 class ServoController:
-    def __init__(self,  servo_sensor_pin=14, thumbturn_sensor_pin=15, servo_pwm_pin=18, rotation_timeout_s=5):
+    def __init__(self, servo_sensor_pin=11, servo_pwm_pin=13, rotation_timeout_s=5):
         """
         GPIOピン番号で指定すること
         """
         self.SERVO_SENSOR_PIN = servo_sensor_pin
-        self.THUMBTURN_SENSOR_PIN = thumbturn_sensor_pin
         self.SERVO_PWM_PIN = servo_pwm_pin
         self.ROTATION_TIMEOUT_S = rotation_timeout_s
 
@@ -35,7 +37,6 @@ class ServoController:
 
         GPIO.setup(self.SERVO_PWM_PIN, GPIO.OUT)
         GPIO.setup(self.SERVO_SENSOR_PIN, GPIO.IN)
-        GPIO.setup(self.THUMBTURN_SENSOR_PIN, GPIO.IN)
 
         #「GPIO4出力」でPWMインスタンスを作成する。
         # GPIO.PWM( [ピン番号] , [周波数Hz] )
@@ -61,15 +62,20 @@ class ServoController:
         https://qiita.com/atmaru/items/2282445d327b0af0e6c1#%E9%85%8D%E7%B7%9A
         """
         self.servo.ChangeDutyCycle(6)
+        detection_counter = 0
         start_time = time.time()
         before_state = GPIO.input(self.SERVO_SENSOR_PIN)
         while time.time() - start_time < self.ROTATION_TIMEOUT_S:
             if GPIO.input(self.SERVO_SENSOR_PIN) == GPIO.LOW:
                 if before_state == GPIO.HIGH:
-                    print("Changed")
-                    break
+                    detection_counter += 1
+                    # HACK: ラズパイのCPU使用率によっても最適な閾値が変わる可能性があるので、信頼性の高い手法に書き換える
+                    if detection_counter > DETECTION_THRESHOLD:
+                        print("Changed")
+                        break
             else:
-                before_state = GPIO.HIGH 
+                before_state = GPIO.HIGH
+                detection_counter = 0
         self.servo.ChangeDutyCycle(0)
         print("[on %s] Complete %s" % (self.__getattribute__.__name__, sys._getframe().f_code.co_name))
     
@@ -80,15 +86,20 @@ class ServoController:
         https://qiita.com/atmaru/items/2282445d327b0af0e6c1#%E9%85%8D%E7%B7%9A
         """
         self.servo.ChangeDutyCycle(9)
+        detection_counter = 0
         start_time = time.time()
         before_state = GPIO.input(self.SERVO_SENSOR_PIN)
         while time.time() - start_time < self.ROTATION_TIMEOUT_S:
             if GPIO.input(self.SERVO_SENSOR_PIN) == GPIO.LOW:
                 if before_state == GPIO.HIGH:
-                    print("Changed")
-                    break
+                    detection_counter += 1
+                    # HACK: ラズパイのCPU使用率によっても最適な閾値が変わる可能性があるので、信頼性の高い手法に書き換える
+                    if detection_counter > DETECTION_THRESHOLD:
+                        print("Changed")
+                        break
             else:
                 before_state = GPIO.HIGH
+                detection_counter = 0
         self.servo.ChangeDutyCycle(0)
         print("[on %s] Complete %s" % (self.__getattribute__.__name__, sys._getframe().f_code.co_name))
 
---
+++ src/smart_lock.py
@@ -0,0 +1,76 @@
+import sys
+
+import RPi.GPIO as GPIO
+from servo_controller import ServoController
+from settings import DETECTION_THRESHOLD
+
+
+def main():
+    s_controller = ServoController()
+    smart_lock = SmartLock()
+    functions = {}
+    functions["cw"] = s_controller.rotate_clockwise
+    functions["ccw"] = s_controller.rotate_counterclockwise
+    functions["ct"] = s_controller.centerlize
+    
+    option = get_options("--operation")
+    if option == "lock":
+        while not smart_lock.is_locked():
+            functions["ccw"]()
+        print("locked")
+        return
+    elif option == "unlock":
+        while smart_lock.is_locked():
+            functions["cw"]()
+        print("unlocked")
+        return
+
+    while True:
+        print("時計回：cw, 反時計回：ccw, 静止位置キャリブレーション：ct")
+        cmd = input()
+        try:
+            functions[cmd]()
+            print("is_locked: %s" % smart_lock.is_locked())
+        except:
+            del s_controller
+            break
+
+
+class SmartLock:
+    def __init__(self,  thumbturn_sensor_pin=17):
+        self.THUMBTURN_SENSOR_PIN = thumbturn_sensor_pin
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.THUMBTURN_SENSOR_PIN, GPIO.IN)
+
+        self.LOCKED_STATUS = GPIO.HIGH
+
+    def is_locked(self):
+        return GPIO.input(self.THUMBTURN_SENSOR_PIN) == self.LOCKED_STATUS
+
+    def __del__(self):
+        pass
+
+
+def get_options(op):
+    """
+    コマンドライン引数のオプションを検索し、オプションの値を取得する
+    Parameters
+    ----------
+    op : string
+        オプション (例："-p"
+    
+    Returns
+    -------
+    op_value : String
+        オプションが見つかったら、オプションの後ろに指定されている値を返す
+        見つからなかったらNoneを返す
+    """
+    args = sys.argv
+    is_find = False  # 当該オプションを見つけた場合はフラグを立てる
+    for val in args:
+        if is_find: return val
+        if val == op: is_find = True
+    return None
+
+if __name__ == "__main__":
+    main()
